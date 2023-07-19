5abf7055db1b6a22d8fa45396455b48b82965d3c
Nick Spencer <strangebrew202@gmail.com>
2023-07-19 09:23:11 -0600

    Add: @passengers attribute, and added passengers test case to #initialize in vehicle_spec

--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -4,11 +4,12 @@ require './lib/passenger'
 describe Vehicle do  
   describe "#initialize" do
     vehicle = Vehicle.new("2001", "Honda", "Civic")  
-    it "has readable year, make and model" do
+    it "has readable year, make and model, empty passenger array at initialize" do
 
       expect(vehicle.year).to eq("2001")
       expect(vehicle.make).to eq("Honda")
       expect(vehicle.model).to eq("Civic")
+      expect(vehicle.passengers).to eq([])
     end
   end
 
