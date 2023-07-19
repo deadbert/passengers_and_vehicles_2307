73ef12226febca292757f93bc37ba7a68bc37b19
Nick Spencer <strangebrew202@gmail.com>
2023-07-19 09:54:11 -0600

    Add: test case for #revenue in park_spec

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -39,8 +39,26 @@ describe Park do
       ford.add_passenger(jude)
       rockies.admit_vehicle(honda)
       rockies.admit_vehicle(ford)
-      
+
       expect(rockies.list_passengers).to eq([charlie, jude])
+    end
+  end
+
+  describe "#revenue" do 
+    rockies = Park.new("Rocky Mountains", 25)
+    honda = Vehicle.new("2001", "Honda", "Civic")
+    ford = Vehicle.new("2001", "Ford", "Fiesta")
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})  
+    jude = Passenger.new({"name" => "Jude", "age" => 20})  
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12}) 
+    it "can calculate revenue based on number of adults that have entered park" do
+      honda.add_passenger(charlie)
+      honda.add_passenger(taylor)
+      ford.add_passenger(jude)
+      rockies.admit_vehicle(honda)
+      rockies.admit_vehicle(ford)
+
+      expect(rockies.revenue).to eq(50)
     end
   end
 
