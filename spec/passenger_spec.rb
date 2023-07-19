2a6415ae2033b340b1be63289fe8e1371503c6ca
Nick Spencer <strangebrew202@gmail.com>
2023-07-19 09:10:58 -0600

    Add: tests for #adult, #drive? and #drive

--- spec/passenger_spec.rb
+++ spec/passenger_spec.rb
@@ -10,4 +10,31 @@ describe Passenger do
       expect(charlie.age).to eq(18)
     end
   end
+
+  describe "#adult?" do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})  
+    it "returns true if passenger.age >= 18" do
+      
+      expect(charlie.adult?).to eq(true)
+      expect(taylor.adult?).to eq(false)
+    end
+  end
+
+  describe "#drive?" do
+    it "returns @driver for passenger" do
+      
+      expect(charlie.drive?).to eq(false)
+    end
+  end
+  describe "#drive" do
+    charlie = Passenger.new({"name" => "Charlie", "age" => 18})    
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})  
+    it "sets @drive to true for passenger" do
+      
+      expect(charlie.driver?).to eq(false)
+      charlie.drive
+      expect(charlie.driver?).to eq(true)
+    end
+  end
 end
